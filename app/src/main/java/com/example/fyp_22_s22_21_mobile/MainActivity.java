6cf819b4bcd4c55e9e9b1f9114538ecd9663a878
Baemee <92077186+Baemee@users.noreply.github.com>
2022-06-20 17:20:50 +0800

    2022.06.20 5:20pm

--- app/src/main/java/com/example/fyp_22_s22_21_mobile/MainActivity.java
+++ app/src/main/java/com/example/fyp_22_s22_21_mobile/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.fyp_22_s22_21_mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn_Login = (Button) findViewById(R.id.btn_Login);
+
+        btn_Login.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), homepage.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
 }
