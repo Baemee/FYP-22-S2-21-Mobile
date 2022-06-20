fe41adc1abb5f5583cc6934450177146bc20d711
Baemee <92077186+Baemee@users.noreply.github.com>
2022-06-20 19:53:47 +0800

    2022.06.20 7:53pm

--- app/src/main/java/com/example/fyp_22_s22_21_mobile/homepage.java
+++ app/src/main/java/com/example/fyp_22_s22_21_mobile/homepage.java
@@ -3,6 +3,7 @@ package com.example.fyp_22_s22_21_mobile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class homepage extends AppCompatActivity {
 
@@ -10,5 +11,14 @@ public class homepage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        Button btn_WaterUsage = (Button) findViewById(R.id.btn_WaterUsage);
+        Button btn_Alerts = (Button) findViewById(R.id.btn_Alerts);
+        Button btn_Bills = (Button) findViewById(R.id.btn_Bills);
+        Button btn_UpdatePassword = (Button) findViewById(R.id.btn_UpdatePassword);
+        Button btn_Report = (Button) findViewById(R.id.btn_Report);
+        Button btn_Logout = (Button) findViewById(R.id.btn_Logout);
+
+
     }
 }
