3bace68f03b277daf3290b1b723d4186d00bc92a
wj <weijie.9876@gmail.com>
2022-07-21 16:26:17 +0800

    Get bills

--- app/src/main/java/com/example/fyp_22_s22_21_mobile/BillDetails.java
+++ app/src/main/java/com/example/fyp_22_s22_21_mobile/BillDetails.java
@@ -2,13 +2,9 @@ package com.example.fyp_22_s22_21_mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,44 +22,42 @@ import org.json.JSONException;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Bills extends AppCompatActivity {
+public class BillDetails extends AppCompatActivity {
 
     SharedPreferences Token;
     String key;
     String url = "http://10.0.2.2:5000/" + "api/Bill/MyInfo";
-    String date;
-    double amount;
+    String billId;
+    int mth;
+    int yr;
     double usage;
-    //String status;
+    double rate;
+    double amount;
+    String deadline;
+    String payment;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bills);
+        setContentView(R.layout.activity_bill_details);
 
         Token = getSharedPreferences("user", MODE_PRIVATE);
         key = "Bearer " + Token.getString("token", String.valueOf(1));
 
-        //Button btn_View = (Button) findViewById(R.id.btn_View_1);
-        TextView tv_billDate = findViewById(R.id.tv_billDate);
-
-        requestBills(url);
-
-        tv_billDate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), BillDetails.class);
-                startActivity(intent);
-            }
-        });
+        requestBillsDetails(url);
     }
 
-    protected void requestBills(String url)
+    protected void requestBillsDetails(String url)
     {
-        TextView tv_billDate = findViewById(R.id.tv_billDate);
-        TextView tv_billAmount = findViewById(R.id.tv_billAmount);
-        TextView tv_billUsage = findViewById(R.id.tv_billUsage);
+        TextView tv_billId = findViewById(R.id.tv_getBillId);
+        TextView tv_billMth = findViewById(R.id.tv_getbillMth);
+        TextView tv_billYr = findViewById(R.id.tv_getBillYr);
+        TextView tv_billUsage = findViewById(R.id.tv_getWaterUsage);
+        TextView tv_rate = findViewById(R.id.getRate);
+        TextView tv_billAmount = findViewById(R.id.tv_getBillAmount);
+        TextView tv_deadline = findViewById(R.id.tv_getDateline);
+        TextView tv_payment = findViewById(R.id.tv_getPayment);
 
         JsonArrayRequest jsonObjectRequest = new JsonArrayRequest(Request.Method.GET, url, null,
                 new Response.Listener<JSONArray>() {
@@ -74,14 +68,27 @@ public class Bills extends AppCompatActivity {
 
                             for(int i = 0; i<length; i++)
                             {
-                                date = response.getJSONObject(i).getString("title");
+                                billId = response.getJSONObject(i).getString("billId");
+                                mth = response.getJSONObject(i).getInt("month");
+                                yr = response.getJSONObject(i).getInt("year");
+                                rate = response.getJSONObject(i).getDouble("rate");
                                 amount = response.getJSONObject(i).getDouble("amount");
                                 usage = response.getJSONObject(i).getDouble("totalUsage");
-                                //status = response.getJSONObject(i).getString("Payment");
+                                deadline = response.getJSONObject(i).getString("deadline");
+                                payment = response.getJSONObject(i).getString("payment");
+                                if (payment.equals("null"))
+                                    payment="Unpaid";
+                                else
+                                    payment="Paid";
 
-                                tv_billDate.append(date+"\n");
+                                tv_billId.append(billId+"\n");
+                                tv_billMth.append(mth+"\n");
+                                tv_billYr.append(yr+"\n");
+                                tv_rate.append(String.format("%.2f",rate)+"\n");
                                 tv_billAmount.append(String.format("%.2f",amount)+"\n");
                                 tv_billUsage.append(String.format("%.2f",usage)+"\n");
+                                tv_deadline.append(deadline+"\n");
+                                tv_payment.append(payment+"\n");
 
                             }
 
@@ -93,7 +100,7 @@ public class Bills extends AppCompatActivity {
             @Override
             public void onErrorResponse(VolleyError error) {
                 Log.e("Response error", error.getMessage());
-                Toast.makeText(Bills.this,
+                Toast.makeText(BillDetails.this,
                                 "An error occurred.",
                                 Toast.LENGTH_LONG)
                         .show();
