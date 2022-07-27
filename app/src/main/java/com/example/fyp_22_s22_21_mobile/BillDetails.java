31fd93632ff6247dec7bbc2c53137bf8da20d452
Baemee <92077186+Baemee@users.noreply.github.com>
2022-07-27 13:43:51 +0800

    2022.07.27

--- app/src/main/java/com/example/fyp_22_s22_21_mobile/BillDetails.java
+++ app/src/main/java/com/example/fyp_22_s22_21_mobile/BillDetails.java
@@ -1,5 +1,6 @@
 package com.example.fyp_22_s22_21_mobile;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -18,6 +19,15 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
+import com.braintreepayments.api.DropInClient;
+import com.braintreepayments.api.DropInListener;
+import com.braintreepayments.api.DropInPaymentMethod;
+import com.braintreepayments.api.DropInRequest;
+import com.braintreepayments.api.DropInResult;
+import com.braintreepayments.api.GooglePayRequest;
+import com.braintreepayments.api.PaymentMethodNonce;
+import com.google.android.gms.wallet.TransactionInfo;
+import com.google.android.gms.wallet.WalletConstants;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -25,8 +35,12 @@ import org.json.JSONException;
 import java.util.HashMap;
 import java.util.Map;
 
-public class BillDetails extends AppCompatActivity {
+public class BillDetails extends AppCompatActivity implements DropInListener {
 
+    //    private static final String SANDBOX_KEY = "sandbox_jy9bjhfx_pctg926vzs477j7t";
+    private static final String SANDBOX_KEY = "sandbox_tmxhyf7d_dcpspy2brwdjr3qn";
+    private static final String MERCHANT_SERVER_URL = "https://sdk-sample-merchant-server.herokuapp.com";
+    private static final String SANDBOX_BASE_SERVER_URL = MERCHANT_SERVER_URL + "/braintree/sandbox";
     SharedPreferences Token;
     String key;
     String url = "http://10.0.2.2:5000/" + "api/Bill/MyInfo";
@@ -38,6 +52,30 @@ public class BillDetails extends AppCompatActivity {
     double amount;
     String deadline;
     String payment;
+
+    private DropInClient dropInClient;
+
+
+    private void createPaymentModule() {
+        DropInRequest dropInRequest = new DropInRequest();
+        dropInRequest.setGooglePayRequest(getGooglePayRequest());
+        dropInRequest.setMaskCardNumber(false);
+
+
+        dropInClient = new DropInClient(this, dropInRequest, SANDBOX_KEY);
+        dropInClient.setListener(this);
+    }
+
+    private GooglePayRequest getGooglePayRequest() {
+        GooglePayRequest googlePayRequest = new GooglePayRequest();
+        googlePayRequest.setTransactionInfo(TransactionInfo.newBuilder()
+                .setTotalPrice("1.00")
+                .setCurrencyCode("USD")
+                .setTotalPriceStatus(WalletConstants.TOTAL_PRICE_STATUS_FINAL)
+                .build());
+        googlePayRequest.setEmailRequired(true);
+        return googlePayRequest;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,16 +99,17 @@ public class BillDetails extends AppCompatActivity {
         btn_payment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), PaymentPage.class);
-                startActivity(intent);
+//                Intent intent = new Intent(getApplicationContext(), PaymentPage.class);
+//                startActivity(intent);
+                dropInClient.launchDropInForResult(BillDetails.this, 1111);
             }
         });
 
         requestBillsDetails(url);
+        createPaymentModule();
     }
 
-    protected void requestBillsDetails(String url)
-    {
+    protected void requestBillsDetails(String url) {
         TextView tv_billId = findViewById(R.id.tv_getBillId);
         TextView tv_billMth = findViewById(R.id.tv_getbillMth);
         TextView tv_billYr = findViewById(R.id.tv_getBillYr);
@@ -87,8 +126,7 @@ public class BillDetails extends AppCompatActivity {
                         try {
                             int length = response.length();
 
-                            for(int i = 0; i<length; i++)
-                            {
+                            for (int i = 0; i < length; i++) {
                                 billId = response.getJSONObject(i).getString("billId");
                                 mth = response.getJSONObject(i).getInt("month");
                                 yr = response.getJSONObject(i).getInt("year");
@@ -99,18 +137,18 @@ public class BillDetails extends AppCompatActivity {
                                 payment = response.getJSONObject(i).getString("payment");
 
                                 if (payment.equals("null"))
-                                    payment="Unpaid";
+                                    payment = "Unpaid";
                                 else
-                                    payment="Paid";
+                                    payment = "Paid";
 
-                                tv_billId.append(billId+"\n");
-                                tv_billMth.append(mth+"\n");
-                                tv_billYr.append(yr+"\n");
-                                tv_rate.append(String.format("%.2f",rate)+"\n");
-                                tv_billAmount.append(String.format("%.2f",amount)+"\n");
-                                tv_billUsage.append(String.format("%.2f",usage)+"\n");
-                                tv_deadline.append(deadline+"\n");
-                                tv_payment.append(payment+"\n");
+                                tv_billId.append(billId + "\n");
+                                tv_billMth.append(mth + "\n");
+                                tv_billYr.append(yr + "\n");
+                                tv_rate.append(String.format("%.2f", rate) + "\n");
+                                tv_billAmount.append(String.format("%.2f", amount) + "\n");
+                                tv_billUsage.append(String.format("%.2f", usage) + "\n");
+                                tv_deadline.append(deadline + "\n");
+                                tv_payment.append(payment + "\n");
 
                             }
                         } catch (JSONException e) {
@@ -126,8 +164,7 @@ public class BillDetails extends AppCompatActivity {
                                 Toast.LENGTH_LONG)
                         .show();
             }
-        })
-        {
+        }) {
             @Override
             public Map<String, String> getHeaders() throws AuthFailureError {
                 HashMap<String, String> headers = new HashMap<String, String>();
@@ -139,4 +176,23 @@ public class BillDetails extends AppCompatActivity {
         RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
         requestQueue.add(jsonObjectRequest);
     }
+
+    @Override
+    public void onDropInSuccess(@NonNull DropInResult dropInResult) {
+
+        PaymentMethodNonce paymentNonce = dropInResult.getPaymentMethodNonce();
+
+        DropInPaymentMethod paymentMethodType = dropInResult.getPaymentMethodType();
+        if (paymentMethodType != null) {
+            Toast.makeText(BillDetails.this, paymentMethodType.getLocalizedName(), Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    @Override
+    public void onDropInFailure(@NonNull Exception e) {
+        Toast.makeText(BillDetails.this, "Failed to make a purchase", Toast.LENGTH_LONG).show();
+    }
+
+
 }
