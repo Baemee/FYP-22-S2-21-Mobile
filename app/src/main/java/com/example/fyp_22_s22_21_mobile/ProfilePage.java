671b4ec4128021ba3b8f0e2fcf1ef2a22e41bc8e
Baemee <92077186+Baemee@users.noreply.github.com>
2022-08-05 09:37:49 +0800

    2022.08.05

--- app/src/main/java/com/example/fyp_22_s22_21_mobile/ProfilePage.java
+++ app/src/main/java/com/example/fyp_22_s22_21_mobile/ProfilePage.java
@@ -10,13 +10,14 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ProfilePage extends AppCompatActivity {
 
     SharedPreferences Token;
-    String username, address, userId, createdAt, password,fullName, gender, email, phone;
+    String username, address, userId, createdAt, fullName, gender, email, phone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +26,33 @@ public class ProfilePage extends AppCompatActivity {
 
         Button btn_pwd = findViewById(R.id.btn_changePwd);
         ImageView img_back = findViewById(R.id.img_back);
+        TextView tv_userId = findViewById(R.id.tv_userId_user);
+        TextView tv_userName = findViewById(R.id.tv_userName_user);
+        TextView tv_fullName = findViewById(R.id.tv_fullName_user);
+        TextView tv_address = findViewById(R.id.tv_address_user);
+        TextView tv_dateCreate = findViewById(R.id.tv_dateCreate_user);
+        TextView tv_Gender = findViewById(R.id.tv_Gender_user);
+        TextView tv_email = findViewById(R.id.tv_email_user);
+        TextView tv_phone = findViewById(R.id.tv_phone_user);
 
         Token = getSharedPreferences("user", MODE_PRIVATE);
+        username = Token.getString("username", String.valueOf(1));
+        address = Token.getString("address", String.valueOf(1));
         userId = Token.getString("userId", String.valueOf(1));
+        createdAt = Token.getString("createdAt", String.valueOf(1));
+        fullName = Token.getString("fullName", String.valueOf(1));
+        gender = Token.getString("gender", String.valueOf("null"));
+        email = Token.getString("email", String.valueOf("null"));
+        phone = Token.getString("phones", String.valueOf("null"));
+
+        tv_userId.setText(userId);
+        tv_userName.setText(username);
+        tv_fullName.setText(fullName);
+        tv_address.setText(address);
+        tv_dateCreate.setText(createdAt);
+        tv_Gender.setText(gender);
+        tv_email.setText(email);
+        tv_phone.setText(phone);
 
         btn_pwd.setOnClickListener(new View.OnClickListener() {
             @Override
